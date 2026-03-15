db91d94993815eff4c0eb86e66c15ba50825d7ba
papaya0481 <923525301@qq.com>
2026-03-15 17:55:41 +0800

    添加config，修改代码以适配新的config

--- dubbing/scripts/train_cfm_index_phase1.sh
+++ dubbing/scripts/train_cfm_index_phase1.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 
-python -m pdb dubbing/run.py \
+accelerate launch --multi_gpu --num_processes=2 dubbing/run.py \
     --config dubbing/configs/default_cfm_index.yaml
